a0e881a28599378e00833d91243756f8029c6fb6
satoshi-tomita <satoshi.tomita@hakuhodo.co.jp>
2018-06-26 07:53:45 +0000

    CRUD tasks

--- resources/views/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <h1>id = {{ $task->id }のメッセージ詳細ページ</h1>
-    
-    <p>{{ $task->content }}</p>
-
-@endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,9 +7,11 @@
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
             @endforeach
         </ul>
     @endif
+    
+    {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
 
 @endsection
