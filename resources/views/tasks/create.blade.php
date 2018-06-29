6f70ab6e9bc273009a395d569c7e0b16ebea0f20
satoshi-tomita <satoshi.tomita@hakuhodo.co.jp>
2018-06-29 08:58:10 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規作成ページ</h1>
     
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        {!! Form::label('content', 'メッセージ：') !!}
+        {!! Form::label('status', 'ステータス：') !!}
+        {!! Form::text('status') !!}
+        
+        {!! Form::label('content', 'タスク内容：') !!}
         {!! Form::text('content') !!}
         
         {!! Form::submit('投稿') !!}
