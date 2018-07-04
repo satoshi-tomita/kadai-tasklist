d61d0a5bb299d55bcc42c521c5b87764c88d8935
satoshi-tomita <satoshi.tomita@hakuhodo.co.jp>
2018-07-04 06:48:57 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,28 +5,23 @@
     <h1>タスク新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-sm-8">
-            <div class=" col-lg-6">
+        <div class="col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+
                 {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
                 <div class="form-group">
-                    {!! Form::label('status', 'ステータス：') !!}
+                    {!! Form::label('status', '進捗状況：') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
-            </div>
-        </div>
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="col-lg-offset-3 col-lg-6">
-                 <div class-"form-group">
+                <div class="form-group">
                     {!! Form::label('content', 'タスク内容：') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
-            </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
         </div>
     </div>
-        
-        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
-        
-    {!! Form::close() !!}
     
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,28 +5,23 @@
     <h1>id: {{ $task->id }}の編集ページ</h1>
     
     <div class="row">
-        <div class="col-sm-8">
-            <div class=" col-lg-6">
+        <div class="col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+
                 {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-                    <div class="form-group">
-                        {!! Form::label('status', '進捗状況：') !!}
-                        {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                    </div>
-            </div>
-        </div>
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="col-lg-offset-3 col-lg-6">
+                       
                 <div class="form-group">
-                        {!! Form::label('content', 'タスク内容：') !!}
-                        {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                    </div>
+                    {!! Form::label('status', '進捗状況：') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク内容：') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
                     
-                    {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
                     
-                {!! Form::close() !!}
-            </div>
+            {!! Form::close() !!}
         </div>
-        
     </div>
     
 @endsection
